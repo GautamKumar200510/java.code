08446d087039be7763860b8775f40d810f83ecdd
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-05-18 20:07:07 +0530

    Update code

--- src/ArrayEx3.java
+++ src/ArrayEx3.java
@@ -20,6 +20,18 @@ avg = sum / length;
 System.out.println("The average age is: " + avg);
 
 
+int[] numbers = {3, -1, 7, 0, 9};
+
+for (int n : numbers) {
+  if (n < 0) {
+    continue; // skip negative numbers
+  }
+  if (n == 0) {
+    break; // stop loop when zero is found
+  }
+  System.out.println(n);
+}
+
     }
 
 }
