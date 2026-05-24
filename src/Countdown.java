e2942462249d445b7348dc069746490c51bb83c3
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-05-24 18:45:59 +0530

    Update code

---
+++ src/Countdown.java
@@ -0,0 +1,13 @@
+public class Countdown {
+    static void countdown(int n) {
+    if (n > 0) {
+      System.out.print(n + " ");
+      countdown(n - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    countdown(5);
+  }
+
+}
