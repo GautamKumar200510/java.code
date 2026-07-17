fd151e1a5b21e35e6d3f9e38d6fcdf1061e6898d
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-07-17 22:50:55 +0530

    Comments

---
+++ src/BasicIntro/Comments.java
@@ -0,0 +1,7 @@
+public class Comments {
+    public static void main(String[] args) {
+        System.out.println("I'm Learning java ");
+       // System.out.println("comments this  line");
+    }
+
+}
