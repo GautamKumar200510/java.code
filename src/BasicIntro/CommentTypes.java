b5115da64d09fd01f27f31d97b633216e9151a1d
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-07-18 23:24:34 +0530

    Learning comment

---
+++ src/BasicIntro/CommentTypes.java
@@ -0,0 +1,19 @@
+public class CommentTypes {
+    public static void main(String[] args) {
+//  1. Single-Line Comment (//)
+    // printing comments
+    System.out.println("Print single line comment "); 
+
+// 2. Multi-Line Comment (/* ... */)
+    /*
+    Hello Java
+    This prints multiline comments 
+     */
+    System.out.println("Print Multi-line comment");
+
+
+
+        
+    }
+
+}
