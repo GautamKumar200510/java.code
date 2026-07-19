037662a128cd271e74e97445c2c4caf53c24ec07
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-07-19 15:36:26 +0530

    Use the Scanner class

--- src/BasicIntro/Hello.java
+++ src/BasicIntro/Hello.java
@@ -1,6 +1,15 @@
+
+import javax.sound.sampled.SourceDataLine;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Java");
-    }
+    
+// Example 2: Printing Variables and Expressions
 
+    int num1 = 10 ,  num2 = 17 ;
+    System.out.print("Addition of ");
+    System.out.print(num1 + " and " + num2 + " is ");
+    System.out.println(num1+num2);
+}
 }
---
+++ src/BasicIntro/UserInput.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first no.");
+       int a= sc.nextInt();
+       System.out.println("Enter second no.");
+       int b = sc.nextInt();
+
+       System.out.println("Sum of two no. is : " +  (a+b) );
+
+
+        
+    }
+
+}
